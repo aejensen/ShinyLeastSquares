18b95c5db7535fd592ae9f87b2fcc022aabc3808
Andreas Jensen <aeje@sund.ku.dk>
2016-10-03 17:58:17 +0200

    simplifying

--- ui.R
+++ ui.R
@@ -3,23 +3,23 @@ library(shiny)
 shinyUI(pageWithSidebar(
   
   # Application title
-  headerPanel("Least squares example"),
+  headerPanel("Eksempel på estimation i simpel lineær regression"),
   
   sidebarPanel(
     sliderInput("alpha", 
-                "Intercept", 
+                "Skæring med y-aksen", 
                 value = 1,
                 min = -1, 
                 max = 3,
                 step = 0.05),
     
     sliderInput("beta", 
-                "Slope", 
+                "Hældningskoefficient", 
                 value = 1,
                 min = -1, 
                 max = 3,
-                step = 0.05),    
-  ),
+                step = 0.05)
+),
   
   mainPanel(
       plotOutput(outputId = "main_plot", height="600px", width="800px")
