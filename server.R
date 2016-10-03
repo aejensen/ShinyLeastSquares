9c4a3cd30004c1f82459053321a008692a1a1c6b
Andreas Jensen <aeje@sund.ku.dk>
2016-10-03 17:49:24 +0200

    simplifying

--- server.R
+++ server.R
@@ -16,29 +16,11 @@ shinyServer(function(input, output) {
     
     abline(a=input$alpha, b=input$beta, lwd=3, col="blue")
     
-    if (input$outputtype=="residuals") {
-      segments(x, input$alpha+input$beta*x, x, y)
-    } else if (input$outputtype=="sqresiduals") {
-      for (i in 1:length(x)) {
-        preds <- input$alpha + input$beta*x[i]
-        ress <- y[i] - preds
-        polygon(x=c(x[i], x[i], x[i]-ress, x[i]-ress), y=c(preds, y[i], y[i], preds),
-                col="#0000BB66")
-      }
-      segments(x, input$alpha+input$beta*x, x, y)
-    }
-    
-    if (input$sse) {        
-        sse <- sum((y - (input$alpha+input$beta*x))**2)
-        bp <- barplot(sse, col="#0000BB66", ylim=c(0, 15), main="Sum of squared residuals")
-        text(bp, 1, sse)
-    }
-    
-#    if (input$density) {
-#      dens <- density(faithful$eruptions,
-#                      adjust = input$bw_adjust)
-#      lines(dens, col = "blue")
-#    }
+    segments(x, input$alpha+input$beta*x, x, y)
+          
+	sse <- sum((y - (input$alpha+input$beta*x))**2)
+    bp <- barplot(sse, col="#0000BB66", ylim=c(0, 15), main="Sum of squared residuals")
+    text(bp, 1, sse)
     
   })
 })
